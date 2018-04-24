ba4162b99fce3b5cf81211a711102ded2a23c499
spharris5200@gmail.com <spharris5200@gmail.com>
2018-04-24 13:18:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var katzDeli = [];
 
 function takeANumber(katzDeliLine, newCustomer) {
   katzDeli.push('${newCustomer}');
-  return('Welcome' + newCustomer + '.' + 'You are number' + katzDeli + 'in line.' );
+  return('Welcome,' + newCustomer + '.' + 'You are number' + katzDeli + 'in line.' );
 }
 
 function nowServing() {
