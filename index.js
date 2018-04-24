90b5745df980472879e8bcfeb6e0b24de2345f5d
spharris5200@gmail.com <spharris5200@gmail.com>
2018-04-24 14:10:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,21 +14,22 @@ function nowServing(katzDeli) {
   
   if (katzDeli.length === 0) {
     return "There is nobody waiting to be served!";
-  }
-  else
-  
-  return (`Currently serving ${katzDeli.shift()}.`);
+  } else
+    return (`Currently serving ${katzDeli.shift()}.`);
 }
+
 var line = [];
 
 function currentLine(katzDeli) {
   let i = 0;
+  
   while (i < katzDeli.length) {
     line.push(` `+[i+1]+`. `  + katzDeli[i]);
     i++;
   }
+  
   if (katzDeli.length === 0) {
     return "The line is currently empty.";
   } else
-  return(`The line is currently:` + line);
+    return(`The line is currently:` + line);
 }
